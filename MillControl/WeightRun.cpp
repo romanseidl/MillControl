e3b6ef06889d4935146d5c611bbf856cb4223c41
Roman Seidl <roman@granul.at>
2016-02-20 14:18:34 +0100

    Offset correct

--- MillControl/WeightRun.cpp
+++ MillControl/WeightRun.cpp
@@ -17,16 +17,32 @@ void WeightRun::loop() {
     if (millis() - start_time > STARTUP_DELAY)
         weight = max(0, UI::scale.get_floating_weight());
 
-    if (weight > (run_data - *calibration_data)) {
+    if (weight > (run_data + *calibration_data)) {
         //Stop Mill etc.
         Run::close();
         //Learn how much the scale is missing out
-        long offset = run_data - UI::scale.get_stable_weight();
+        long weight = UI::scale.get_stable_weight();
+        long offset = run_data - weight;
+/*        Serial.begin(9600);
+        Serial.print("\n");
+        Serial.print(run_data);
+        Serial.print(",");
+        Serial.print(weight);
+        Serial.print(",");
+        Serial.print(offset);
+*/
         //only believe offsets up to 0.5 grams
-        if (offset < 5 && offset > -5) {
+        if (offset < 10 && offset > -10) {
+/*            Serial.print(",");
+            Serial.print(*calibration_data);
+*/
             *calibration_data = *calibration_data + (offset / 2);
+/*            Serial.print(",");
+            Serial.print(*calibration_data);
+*/
             MillControl::TIME_MODE_SELECTOR.timeModes.eepromWrite();
         }
+//        Serial.flush();
         UI::scale.power_down();
     }
     else
